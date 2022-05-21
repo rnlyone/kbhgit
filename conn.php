412867a25a0914ceeaa138e41cd4310e93c12762
rnlyone <ruzmaneman@gmail.com>
2022-05-21 23:10:45 +0800

    single button submit

--- conn.php
+++ conn.php
@@ -2,8 +2,8 @@
 // print_r($_POST['dbuser']);
 $servername = "localhost";
 $database = "yoctobra_jurnalx";
-$username = "root";
-$password = "";
+$username = "yoctobra_articlecheck";
+$password = "AQW9ej(w87C:7i";
 
 // ($database, $username, $password);
 
