d9a0235d307b325da0de76122c51bf831b8a29b1
rnlyone <ruzmaneman@gmail.com>
2022-05-22 18:43:30 +0800

    bug dbconn1

--- update.php
+++ update.php
@@ -32,16 +32,19 @@ try {
                 
             } else if ($tabell == 'submissions') {
                 $data2 = str_replace('T', ' ', $data2);
-                $sql = "UPDATE submissions SET date_submitted = '".$data1."', date_status_modified = '".$data2."' where submission_id ='".$id."'";
+                $stmt = "UPDATE submissions SET date_submitted =?, date_status_modified=? where submission_id=?";
+                $stmt = $conn->prepare($stmt);
+                $stmt->bind_param('ssi', $data1, $data2, $id);
+                // $sql = "UPDATE submissions SET date_submitted = '".$data1."', date_status_modified = '".$data2."' where submission_id ='".$id."'";
                 // echo $sql;
                 // $query = $conn->query($sql);
             }
     
             try {
-                $query = $conn->query($sql);
+                $stmt->execute();
                 $check = $conn -> stat();
                 if($query === false) {
-                    echo $conn->error . $conn->errno . $sql . $key;
+                    echo $conn->error . $conn->errno . $stmt . $key;
                     exit;
                 }
                 header('Location:'.$_SERVER['HTTP_REFERER'].'&status="'.$check.'"');
